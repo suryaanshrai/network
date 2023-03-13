da683328d48febe3817a23f78abbd8a78125dd81
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-03-13 06:02:24 +0000

    Mon, Mar 13, 2023, 11:32 AM +05:30

--- network/static/network/reactui.js
+++ network/static/network/reactui.js
@@ -14,18 +14,22 @@ function loadPage(pageName) {
 }
 
 function AllPosts(props) {
-    loadPage('AllPosts2');
+    const [count, setCount] = useState(0);
 
-    const [allPosts, setAllPosts] = React.useState(0)
-    
-    fetch('/getAllPosts')
-    .then(response => response.json())
-    .then(data => {
-        console.log('i reached here');
-    });
-    return (
-            <p>{allPosts}</p>
-    )
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
 ReactDOM.render(<AllPosts />, document.querySelector('#AllPosts2'));
