559860e0f25959a1251c6f6ca73d75bb93bbb65c
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-03-13 06:06:18 +0000

    Mon, Mar 13, 2023, 11:36 AM +05:30

--- network/static/network/reactui.js
+++ network/static/network/reactui.js
@@ -14,10 +14,12 @@ function loadPage(pageName) {
 }
 
 function AllPosts(props) {
-    const [count, setCount] = useState(0);
+
+    loadPage('AllPosts2');
+    const [count, setCount] = React.useState(0);
 
   // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
+  React.useEffect(() => {
     // Update the document title using the browser API
     document.title = `You clicked ${count} times`;
   });
