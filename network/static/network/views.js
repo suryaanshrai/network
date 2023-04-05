deb94278e8ea49cacf77fa104aa8a73b4a7bec6a
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-04-05 06:09:16 +0000

    Wed, Apr 5, 2023, 11:39 AM +05:30

--- network/static/network/views.js
+++ network/static/network/views.js
@@ -101,6 +101,6 @@ function loadPosts(data) {
 
         if (data.pagecount > 1) {
             paginator=document.querySelector("#paginator");
-            paginator.innerHTML = "<nav aria-label=\"Page navigation example\"><ul class=\"pagination\"><li class=\"page-item\"><a class=\"page-link\" href=\"#\">Previous</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">1</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">2</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">3</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">Next</a></li></ul></nav>"
+            paginator.innerHTML = "<ul class=\"pagination\"><li class=\"page-item\"><a class=\"page-link\" href=\"#\">Previous</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">1</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">2</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">3</a></li><li class=\"page-item\"><a class=\"page-link\" href=\"#\">Next</a></li></ul>"
         }
 }
