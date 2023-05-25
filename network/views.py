a93b6899fa009b9a398fc1bd0ee76f43f4207e87
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-05-25 19:48:12 +0000

    Fri, May 26, 2023, 1:18 AM +05:30

--- network/views.py
+++ network/views.py
@@ -90,6 +90,8 @@ def getAllPosts(request):
     allPosts.reverse()
     paginator = Paginator(allPosts, 10)
     page_no = request.GET.get('page')
+    if page_no is None:
+        page_no = 0
     page_obj = list(paginator.get_page(page_no))
     return JsonResponse({
         "allPosts": page_obj,
