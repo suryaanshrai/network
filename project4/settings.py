8ad557cc9c9a0023ccac0651d4e174f31e454d20
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-06 16:52:23 +0000

    Tue, Jun 6, 2023, 10:22 PM +05:30

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://suryaanshrai-code50-102371942-g4wxv754jfpp99-8000.preview.app.github.dev']
+ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = ['https://suryaanshrai-code50-102371942-g4wxv754jfpp99-8000.preview.app.github.dev']
 
 
 # Application definition
