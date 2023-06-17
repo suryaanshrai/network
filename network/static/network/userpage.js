b5a62a890946145d7465b4630fdbcccad5e8e3b2
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-17 20:32:45 +0000

    Sun, Jun 18, 2023, 2:02 AM +05:30

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -1,12 +1,30 @@
-function LikePost(id,csrftoken) {
-    fetch(`likePost/${post.id}`, {
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function LikePost(id) {
+
+    const csrftoken = getCookie('csrftoken');
+
+    fetch(`likePost/${id}`, {
         method:"POST",
         headers: {'X-CSRFToken':csrftoken}
     })
     .then(likereponse=>likereponse.json())
     .then(likedata => {
         console.log(likedata);
-        document.querySelector(`#post${post.id}`).innerHTML = `Likes: ${likedata.newlikecount}`;
+        document.querySelector(`#post${id}`).innerHTML = `Likes: ${likedata.newlikecount}`;
     })
     return false;
 }
