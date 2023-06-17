639ee1157a5d8e4a86a30e78902482dc2025e01b
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-17 20:30:19 +0000

    Sun, Jun 18, 2023, 2:00 AM +05:30

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -1,12 +1,12 @@
-function LikePost(id) {
-    console.log(fetch(`likePost/${id}`, {
-        method: 'POST',
-        body: new FormData(document.querySelector(`\"#${id}form\"`))
+function LikePost(id,csrftoken) {
+    fetch(`likePost/${post.id}`, {
+        method:"POST",
+        headers: {'X-CSRFToken':csrftoken}
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    }));
-
+    .then(likereponse=>likereponse.json())
+    .then(likedata => {
+        console.log(likedata);
+        document.querySelector(`#post${post.id}`).innerHTML = `Likes: ${likedata.newlikecount}`;
+    })
     return false;
 }
