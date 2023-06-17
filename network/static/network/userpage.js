3a78af391f1fdaa1b6e2cceafd97660b2e0c02e1
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-17 20:27:53 +0000

    Sun, Jun 18, 2023, 1:57 AM +05:30

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -1,12 +1,12 @@
 function LikePost(id) {
-    console.log("Happy Me");
-    fetch(`likePost/${id}`, {
+    console.log(fetch(`likePost/${id}`, {
         method: 'POST',
-        body: new FormData(document.querySelector(`#${id}form`))
+        body: new FormData(document.querySelector(`\"#${id}form\"`))
     })
     .then(response => response.json())
     .then(data => {
         console.log(data);
-    });
+    }));
+
     return false;
 }
