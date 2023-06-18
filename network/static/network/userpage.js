09f103a1dee78a2cbec926b7d7c1b55c5447a312
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-18 08:00:25 +0000

    Sun, Jun 18, 2023, 1:30 PM +05:30

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -45,55 +45,6 @@ function LikePost(id) {
     return false;
 }
 
-let edit = document.createElement('div');
-                let editButton = document.createElement('button');
-                let editSubmit = document.createElement('button');
-                let editText = document.createElement('textarea');
-                let editForm = document.createElement('form');
-
-                editText.style.display = "none";
-                editText.name="post_content";
-                editButton.innerHTML="Edit";
-                editButton.onclick=()=> {
-                    editText.style.display="block";
-                    editSubmit.style.display="block";
-                    editButton.style.display="none";
-                    editText.innerHTML=post.content;
-                    document.querySelector(`#toHide${post.id}`).style.display="none";
-                    likeForm.style.display="none";
-                }
-
-                editButton.classList.add('btn', 'btn-primary', 'btn-sm');
-                editSubmit.classList.add('btn', 'btn-primary', 'btn-sm');
-                editSubmit.style.display="none";
-                editSubmit.innerHTML="Done";
-                editForm.onsubmit=()=> {
-                    return false;
-                }
-                editSubmit.onclick= () => {
-                    fetch(`/editpost/${post.id}`, {
-                        method:"POST",
-                        body: new FormData(editForm),
-                        headers: {
-                            'X-CSRFToken': csrftoken
-                        }
-                    })
-                    .then(editresponse => editresponse.json())
-                    .then(editdata => {
-                        console.log(editdata);
-                        document.querySelector(`#toupdate${post.id}`).innerHTML=editdata.post_content;
-                        editText.style.display="none";
-                        editSubmit.style.display="none";
-                        editButton.style.display="block";
-                        document.querySelector(`#toHide${post.id}`).style.display="block";
-                        likeForm.style.display="block";
-                    });
-                    return false;
-                }
-                editForm.append(editText, editSubmit);
-                edit.append(editButton,editForm);
-                thispost.append(likeForm);
-                let user_id=document.querySelector("#userid").value;
-                if (post.poster_id == user_id) {
-                    thispost.append(edit);
-                }
+function editpost(id) {
+    document.querySelector(`toHide${id}`).style.display="none";
+}
